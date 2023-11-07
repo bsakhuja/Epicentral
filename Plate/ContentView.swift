7c6df90f584f26bd04e84a1e16b8d417a21da4da
Brian Sakhuja <briansakhuja@gmail.com>
2023-11-07 10:27:51 -0800

    Initial Commit

---
+++ Plate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plate
+//
+//  Created by Brian Sakhuja on 11/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plate/PlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateApp.swift
+//  Plate
+//
+//  Created by Brian Sakhuja on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
