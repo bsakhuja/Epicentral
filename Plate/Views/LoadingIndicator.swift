4705eeda11175ca31212f76e5f313e25700ca842
Brian Sakhuja <briansakhuja@gmail.com>
2023-11-09 15:16:55 -0800

    fix date issue, add loading indicator

---
+++ Plate/Views/LoadingIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingIndicator.swift
+//  Plate
+//
+//  Created by Brian Sakhuja on 11/9/23.
+//
+
+import SwiftUI
+
+struct LoadingIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingIndicator()
+}
